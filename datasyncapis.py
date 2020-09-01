4a4c8c02137a87339376a9e5b7c6acf556fa3840
pythexcel <pythexcel@excellencetechnologies.in>
2020-09-01 10:35:56 +0530

    response check

--- datasyncapis.py
+++ datasyncapis.py
@@ -3,42 +3,61 @@ from locust import HttpUser, task, between,TaskSet
 import config
 
 
-
 class DataSyncApisLoadTesting(TaskSet):
     wait_time = between(1, 2)
 
+
+    @task
+    def datasyncaccountname(self):
+        response = self.client.get("/datasync/filter/fetch/5e86bdab718bf237a97fefe2/job_profile/0?account-name="+str(config.account_name)+"&accessToken="+str(config.access_token)+"")
+        assert response.status_code == 200
+        assert response
+            
     @task
     def datasync(self):
-        self.client.get("/datasync/filter/fetch/5e86bdab718bf237a97fefe2/job_profile/0/5e86bdab718bf237a97fefe5/True?account-name="+str(config.account_name)+"&accessToken="+str(config.access_token)+"")
-    
+        response = self.client.get("/datasync/filter/fetch/5e86bdab718bf237a97fefe2/job_profile/0/5e86bdab718bf237a97fefe5/True?account-name="+str(config.account_name)+"&accessToken="+str(config.access_token)+"")
+        assert response.status_code == 200
+        assert response
+
     @task
     def JobProfileDatasync(self):
-        self.client.get("/datasync/job-profile/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/job-profile/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
+
 
     @task
     def FilterExperiance(self):
-        self.client.get("/datasync/filter/get/experiance/display?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/filter/get/experiance/display?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def FilterEducationByDegree(self):
-        self.client.get("/datasync/filter/get/education/display/B.tech?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/filter/get/education/display/B.tech?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def FilterEducation(self):
-        self.client.get("/datasync/filter/get/education/display?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/filter/get/education/display?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def FilterEducationfull(self):
-        self.client.get("/datasync/filter/get/education/display/full?account-name="+str(config.account_name)+"")
-
-    @task
-    def classifymoved(self):
-        self.client.get("/datasync/classify-moved/5f3e393da226750039dc36c7/HR/softwaredevelopment?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/filter/get/education/display/full?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def profilemoved(self):
-        self.client.get("/datasync/job-profile-moved/5f3e393da226750039dc36c7/5e86bdab718bf237a97fefe2/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/job-profile-moved/5f3e393da226750039dc36c7/5e86bdab718bf237a97fefe2/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def datasyncdulldata(self):
-        self.client.get("/datasync/full?account-name="+str(config.account_name)+"")
+        response = self.client.get("/datasync/full?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200  
+        assert response
--- skillsapis.py
+++ skillsapis.py
@@ -8,20 +8,30 @@ class SkillsApisLoadTesting(TaskSet):
 
     @task
     def Search(self):
-        self.client.get("/search/search/html?account-name="+str(config.account_name)+"")
+        response = self.client.get("/search/search/html?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def skills(self):
-        self.client.get("/skill/css?account-name="+str(config.account_name)+"")
+        response = self.client.get("/skill/css?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def SkillsMatchbylang(self):
-        self.client.get("/skill/match/css?account-name="+str(config.account_name)+"")
+        response = self.client.get("/skill/match/css?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def SkillsMatch(self):
-        self.client.get("/skillextract/all:5e86bdab718bf237a97fefe2:1/html 5,css_3,angular_js?account-name="+str(config.account_name)+"")
+        response = self.client.get("/skillextract/all:5e86bdab718bf237a97fefe2:1/html 5,css_3,angular_js?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
 
     @task
     def getJobStatus(self):
-        self.client.get("/resume/getJobStatus/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        response = self.client.get("/resume/getJobStatus/5e86bdab718bf237a97fefe2?account-name="+str(config.account_name)+"")
+        assert response.status_code == 200
+        assert response
